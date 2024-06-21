a60b3a05827bdf3dd55bde491ae22331d665f561
Sérgio da Rocha <sergiorajr1994@gmail.com>
2024-06-21 12:33:38 -0300

    ajuste nome

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sérgio da Rocha.
+          Sérgio da Rocha
         </p>
       </header>
     </div>
