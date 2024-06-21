0907203a8e8ee728e9d09ee61b1e134e20a7dc0d
Leila Brum <contato.leilabrum@gmail.com>
2024-06-21 12:53:02 -0300

    corrigindo denovo e salvando

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,15 @@ function App() {
       <header className="App-header">j
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-<<<<<<< HEAD
           Leila Brum
         </p>
         <p>
-=======
-Marina Camaño
+        Marina Camaño
         </p>
-
+        <p>
           Gabriel Dantas
         </p>
         <p>
-          Ricardo Gumiero
->>>>>>> 0f58fa968fe0f27722734f55592c8ea79e4282c3
           Sérgio da Rocha
         </p>
         <p>
