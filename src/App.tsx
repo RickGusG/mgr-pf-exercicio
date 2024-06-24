b194d1c7776325f297adf4d6ca34044ce3a5884f
Alexandre Velozo <alexandrevelozo4@gmail.com>
2024-06-24 15:28:48 -0300

    feat: organização

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,8 @@
-
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <p>
-          Ricardo Gumiero
-        </p>
-        <p>
-          Caio Oliveira
-        </p>
-        <p>
-          Palloma Nobre 
-        </p>
-
-    
-        <p>
-          Natan Costa
-        </p>
-
-        <p>
-          Leila Brum
-        </p>
-
-        <p>
-          Marina Camaño
-        </p>
-          
-          <p>
-          Sérgio da Rocha
-          </p>
-          
-      
-
-        <p>
-          Pedro Vitor Rodrigues Coelho
-        </p>
-        <p>
-          Thalles Palmarim
-         </p>
-         <p>
-          Gabriel Dantas
-      </p>
-        <p>
-          Marcelly
-        </p>
-
-        <p>
-          Alexandre Junior
-          </p>
-        <p>
-          Thaisa Souza
-        </p>
-
-        
-        <p>
-          Natanael Santana
-        </p>
-       
-      </header>
-    </div>
+    <>
+      <h1>Landing Page</h1>
+    </>
   );
 }
 
