543788eb8cbf835496a400ed5362095dd3545030
Thalles Palmarim <thallesrpaugusto@gmail.com>
2024-06-24 16:10:59 -0300

    Nossa historia e rodape

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import NossaHistoria from "./components/NossaHistoria";
+import Rodape from "./components/Rodape";
+
 function App() {
   return (
     <>
       <h1>Landing Page</h1>
+      <NossaHistoria></NossaHistoria>
+      <Rodape></Rodape>
     </>
   );
 }
---
+++ src/components/NossaHistoria/index.jsx
@@ -0,0 +1,17 @@
+import './NossaHistoria.css'
+
+const NossaHistoria = () => {
+    return (
+        <div className='NossaHistoria'>
+        <h2 className='h2NH'>Nossa história</h2>
+        <p className='pNH'>Em 17 de junho de 2024, um sonho se tornou realidade: iniciamos o processo de formação da ModalGR como estagiarios.</p>
+  <p className='pNH'>
+ Dentre os 244 inscritos, apenas 15 foram selecionados. Esse grupo seleto, conhecido como "Os 6%", representa a porcentagem dos profissionais contratados em suas áreas de atuação: UI/UX, desenvolvimento fullstack, garantia da qualidade (QA) e infraestrutura. Mais do que habilidades técnicas impecáveis, "Os 6%" se destacam pela paixão pelo que fazem e pela sinergia com a cultura e os valores da ModalGR.</p>
+  <p className='pNH'>
+ Desde o início, a união e o entusiasmo do grupo foram contagiantes. Juntos, formamos um time coeso e pronto para enfrentar os desafios que estão por vir. Cada um de nós está ansioso para contribuir para o crescimento da empresa e fazer a diferença no mercado.</p>
+  <p className='pNH'>
+ Somos "Os 6%", e juntos, somos a força que impulsiona a inovação. #Pracima</p>
+        </div>
+    )
+}
+export default NossaHistoria
