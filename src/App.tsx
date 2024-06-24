eeda263fdfab40a764105bc74175063d27892e22
Marcelly Farias <marcelly.farias@fatec.sp.gov.br>
2024-06-24 16:06:23 -0300

    aaaaa

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 function App() {
   return (
-<<<<<<< HEAD
 
-   
    
    
    
@@ -20,71 +18,7 @@ function App() {
       <h1>Landing Page</h1>
     </>
 
-=======
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <p>
-          Ricardo Gumiero
-        </p>
-        <p>
-          Caio Oliveira
-        </p>
-        <p>
-          Palloma Nobre 
-        </p>
 
-    
-        <p>
-          Natan Costa
-        </p>
-
-        <p>
-          Leila Brum
-        </p>
-
-        <p>
-          Marina Camaño
-        </p>
-          
-          <p>
-          Sérgio da Rocha
-          </p>
-          
-      
-
-        <p>
-          Pedro Vitor Rodrigues Coelho
-        </p>
-        <p>
-          Thalles Palmarim
-         </p>
-         <p>
-          Gabriel Dantas
-      </p>
-      <p>
-        Ricardo Gumiero
-        </p>
-        <p>
-          Marcelly
-        </p>
-
-        <p>
-          Alexandre Junior
-          </p>
-        <p>
-          Thaisa Souza
-        </p>
-
-        
-        <p>
-          Natanael Santana
-        </p>
-       
-      </header>
-    </div>
->>>>>>> b32e10e7021552760104b7a2f7ebec5b9d98a109
   );
 }
 
